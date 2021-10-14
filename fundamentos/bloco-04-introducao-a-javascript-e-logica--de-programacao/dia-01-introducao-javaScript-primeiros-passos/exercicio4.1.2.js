fa460c290c626fa51f59d15a987533baa7a8cc81
TonLima <tondrums@hotmail.com>
2021-10-14 17:39:30 -0300

    If e else

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-01-introducao-javaScript-primeiros-passos/exercicio4.1.2.js
@@ -0,0 +1,8 @@
+const notaPessoaCandidata = 75;
+
+if (notaPessoaCandidata >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (notaPessoaCandidata < 80 || notaPessoaCandidata >= 60) {
+  console.log("Você está na lista de espera");
+} else if (notaPessoaCandidata < 60);
+console.log("Você foi reprovado");
