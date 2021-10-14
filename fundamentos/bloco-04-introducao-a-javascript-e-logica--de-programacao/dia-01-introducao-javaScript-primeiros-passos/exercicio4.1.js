0f65da0f0aaba73360289d219941d42e6daa2da9
TonLima <tondrums@hotmail.com>
2021-10-14 17:13:40 -0300

    Atualizando o exercicio

--- fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-01-introducao-javaScript-primeiros-passos/exercicio4.1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-01-introducao-javaScript-primeiros-passos/exercicio4.1.js
@@ -2,9 +2,9 @@ const base = 5;
 
 let height = 8;
 
-const area = 5 * 8;
+const area = base * height;
 
-const perimeter = 5 + 8;
+const perimeter = 5 + 8 + 8 + 5;
 
 console.log(area);
 console.log(perimeter);
