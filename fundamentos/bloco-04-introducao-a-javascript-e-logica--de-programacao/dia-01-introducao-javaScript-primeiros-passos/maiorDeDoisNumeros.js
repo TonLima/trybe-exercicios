43c84ff35042a96474363478196bc0ea64e7dfbd
TonLima <tondrums@hotmail.com>
2021-10-15 20:36:54 -0300

    Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-01-introducao-javaScript-primeiros-passos/maiorDeDoisNumeros.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-01-introducao-javaScript-primeiros-passos/maiorDeDoisNumeros.js
@@ -2,7 +2,7 @@ const number1 = 35;
 
 const number2 = 40;
 
-if (number1 > 40) {
+if (number1 > number2) {
   console.log("'number1' É maior que 'number2'");
 } else {
   console.log("'number2' É maior que 'number1'");
