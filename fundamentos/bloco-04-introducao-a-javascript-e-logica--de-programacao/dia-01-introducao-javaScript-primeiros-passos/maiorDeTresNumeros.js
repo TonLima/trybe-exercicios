65cc441ca012175e2ec99d1d829b15b380be6641
TonLima <tondrums@hotmail.com>
2021-10-15 22:23:04 -0300

    Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-01-introducao-javaScript-primeiros-passos/maiorDeTresNumeros.js
@@ -0,0 +1,13 @@
+const num1 = 35;
+
+const num2 = 32;
+
+const num3 = 40;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("O maior número é:" + num1 + "(num1)");
+} else if (num2 > num1 && num2 > num3) {
+  console.log("O maior número é:" + num2 + "(num2)");
+} else {
+  console.log("O maior número é:" + num3 + "(num3)");
+}
