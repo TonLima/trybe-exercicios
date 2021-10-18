0b6a3d339169c7da21ca4c97720715efd28ced7d
TonLima <tondrums@hotmail.com>
2021-10-18 18:58:25 -0300

    Como adicionar mais informaçoẽs no meu array

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-02-JavaScript-Array-e-loop-For/exemplo3.js
@@ -0,0 +1,5 @@
+let minhaLista = ["Tomar café", "Reunião", "Brincar com o cachorro"];
+
+minhaLista.push("Fazer os exercicios da Trybe"); // Aqui foi adicionado mais uma informação no meu array
+
+console.log(minhaLista);
