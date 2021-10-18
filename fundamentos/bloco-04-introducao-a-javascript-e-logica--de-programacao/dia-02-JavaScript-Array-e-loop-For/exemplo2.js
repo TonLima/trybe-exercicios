b7f14feddfd4695638c95021b86dcdaa374236af
TonLima <tondrums@hotmail.com>
2021-10-18 18:17:37 -0300

    Mostrando minha primeira ação do dia e a última informação do meu array

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-02-JavaScript-Array-e-loop-For/exemplo2.js
@@ -0,0 +1,7 @@
+let minhaLista = ["Tomar café", "Reunião", "Brincar com o cachorro"];
+
+let primeiraAcao = minhaLista[0]; // Aqui foi criado uma variável para identificar a minha primeira ação do dia.
+console.log(primeiraAcao); //Aqui exibe o resultado de Tomar café.
+
+let primeiraAcao = minhaLista[minhaLista.length - 1]; //Aqui busca a última informçao do meu array.
+console.log(primeiraAcao); //Aqui exibe o resultado Brincar com o cachorro.
