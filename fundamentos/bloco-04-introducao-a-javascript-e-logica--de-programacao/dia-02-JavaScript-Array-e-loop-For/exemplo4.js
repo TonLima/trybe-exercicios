f0de02778630f77db8f74f6c097ae03da96d5c36
TonLima <tondrums@hotmail.com>
2021-10-18 19:03:47 -0300

    Usando o comando unshift para adicionar uma informção no primeiro índice do seu array

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-02-JavaScript-Array-e-loop-For/exemplo4.js
@@ -0,0 +1,5 @@
+let minhaLista = ["Tomar café", "Reunião", "Brincar com o cachorro"];
+
+minhaLista.unshift("Acordar e abrir os olhos"); // Esse comando vc adiciona uma informção no primeiro indice do seu array.
+
+console.log(minhaLista);
