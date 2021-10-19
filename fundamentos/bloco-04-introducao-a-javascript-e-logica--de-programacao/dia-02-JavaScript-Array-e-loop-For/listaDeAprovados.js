fea15a057c55740c7db51211b7c87acaaf073576
TonLima <tondrums@hotmail.com>
2021-10-19 16:06:44 -0300

    Criando uma lista de aprovados usando a estrutura de repetição For

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-02-JavaScript-Array-e-loop-For/listaDeAprovados.js
@@ -0,0 +1,5 @@
+let listaDeNomes = ["Washington", "Rute", "Isabella", "Heitor"];
+for (let indice = 0; indice < listaDeNomes.length; indice++) {
+  let mensagem = "Sejam Bem Vindos, " + listaDeNomes[indice] + "!";
+  console.log(mensagem);
+}
