5a3ac7fe527ccc9c629dc2ac7c8ea375ff84b9bc
TonLima <tondrums@hotmail.com>
2021-10-20 13:58:29 -0300

    Atualização de nomes

--- fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-03-JavaScript - Lógica -de- Programação -e- Algoritmos/exercicio1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-03-JavaScript - Lógica -de- Programação -e- Algoritmos/exercicio1.js
@@ -1,9 +1,10 @@
 let n = 5;
-let x = "";
-for (let linha = 1; linha <= n; linha += 1) {
+let linha = "";
+
+for (let index = 1; index <= n; index += 1) {
   for (let coluna = 1; coluna <= n; coluna += 1) {
-    x += "*";
+    linha += "*";
   }
-  console.log(x);
-  x = "";
+  console.log(linha);
+  linha = "";
 }
