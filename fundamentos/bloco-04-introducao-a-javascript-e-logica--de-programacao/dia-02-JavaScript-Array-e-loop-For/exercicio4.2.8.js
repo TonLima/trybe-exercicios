06208d2bb66de0f079dadeb59af3cb236794f942
TonLima <tondrums@hotmail.com>
2021-10-20 11:05:28 -0300

    Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-02-JavaScript-Array-e-loop-For/exercicio4.2.8.js
@@ -0,0 +1,15 @@
+//let array = [
+//1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+//23, 24, 25,
+//];
+
+//for (let numero of array) {
+//console.log(numero);
+//}
+
+let arrayNumbers = [];
+
+for (let i = 1; i <= 25; i += 1) {
+  arrayNumbers.push(i);
+}
+console.log(arrayNumbers);
