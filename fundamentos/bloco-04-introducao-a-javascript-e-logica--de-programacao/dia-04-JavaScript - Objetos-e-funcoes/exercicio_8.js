4cd87f319af64ec8aea23f208403775f1146d60a
TonLima <tondrums@hotmail.com>
2021-10-21 10:25:08 -0300

    Crie uma função que receba um array de inteiros e retorne o índice do menor valor.

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-04-JavaScript - Objetos-e-funcoes/exercicio_8.js
@@ -0,0 +1,11 @@
+function indiceMenor(numeros) {
+  let indiceMenor = 0;
+
+  for (let indice in numeros) {
+    if (numeros[indiceMenor] > numeros[indice]) {
+      indiceMenor = indice;
+    }
+  }
+  return indiceMenor;
+}
+console.log(indiceMenor([2, 4, 6, 7, 10, 0, -3]));
