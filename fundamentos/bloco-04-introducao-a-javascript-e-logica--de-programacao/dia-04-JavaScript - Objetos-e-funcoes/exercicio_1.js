3593f20cfbe81c8cd2a6f32d6e230d576b239f4e
TonLima <tondrums@hotmail.com>
2021-10-21 08:40:00 -0300

    Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:

--- fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-04-JavaScript - Objetos-e-funcoes/exercicio_1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-04-JavaScript - Objetos-e-funcoes/exercicio_1.js
@@ -3,4 +3,7 @@ let info = {
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
+/*Imprima no console uma mensagem de boas-vindas para a personagem 
+acima, incluindo seu nome. Valor esperado no console:*/
+
 console.log(" Bem-vinda " + info.personagem + " ");
