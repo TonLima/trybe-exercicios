a3f3859eee76a58554613fc2ec89c8d60eefb734
TonLima <tondrums@hotmail.com>
2021-10-21 10:15:05 -0300

    Crie uma função que receba um array de inteiros e retorne o índice do maior valor.

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica--de-programacao/dia-04-JavaScript - Objetos-e-funcoes/exercicio_7.js
@@ -0,0 +1,12 @@
+function indiceMaior(numeros) {
+  let indiceMaior = 0;
+
+  for (let indice in numeros) {
+    if (numeros[indiceMaior] < numeros[indice]) {
+      indiceMaior = indice;
+    }
+  }
+  return indiceMaior;
+}
+
+console.log(indiceMaior([2, 3, 6, 7, 10, 1]));
