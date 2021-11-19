37609f9bbff792ea82117c63aa5b23eff1c4a20e
TonLima <tondrums@hotmail.com>
2021-11-19 16:37:59 -0300

    Exemplos com template literals e sem template literals

---
+++ fundamentos/bloco-07-introducao-a-javaScript-ES6-e-Testes-Unitarios/dia-01-var-let-const-arrowFunctions-e-templateLiterals/parte_2.js
@@ -0,0 +1,17 @@
+// template literals
+
+/*const myName = "Isabella";
+
+console.log("Hello" + " " + myName + "!");*/
+
+/*const myName = "Isabella";
+
+console.log(`Welcome ${myName}!`); // A sintaxe do template literals pede para usarmos o sinal de crases no início e no final da frase, e variáveis dentro de um ${...}*/
+
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`);
+
+//Sem o template literals:
+console.log("Primeira linha;\n" + "Segunda linha;\n" + "Terceira linha;\n");
