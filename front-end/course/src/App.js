cd2aca11ffdd686db088c83e7c3b379af96ac4c5
TonLima <tondrums@hotmail.com>
2022-02-11 15:57:01 -0300

    Testando classes e funções em React

---
+++ front-end/course/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MiniPortifolio from './MiniPortifolio';
+import './App.css';
+
+function App() {
+  return <MiniPortifolio />
+};
+
+export default App;
+
---
+++ front-end/course/src/MiniPortifolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MiniPortifolio extends React.Component () {
+    render(){
+        return  <>
+        <h1>Washington Lima Ferreira</h1>
+        <p>Sou casado e tenho 39 anos.</p>
+        </>
+    }
+}
+
+export default MiniPortifolio;
