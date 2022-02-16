48ceb4a31f82fd60a6df30823161c4810a71f7b4
TonLima <tondrums@hotmail.com>
2022-02-16 10:31:39 -0300

    Exercicios do bloco 10 introdução ao react

---
+++ front-end/Aula-10.1-exercicio_1-React/exercicio-3/exercicio-3-course/src/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Header extends Component {
+  render() {
+    return(
+      <div>
+        <h1 className='title'>Conteúdos de Front-End</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
