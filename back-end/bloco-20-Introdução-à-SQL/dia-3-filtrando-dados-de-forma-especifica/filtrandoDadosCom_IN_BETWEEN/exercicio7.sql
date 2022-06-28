8415ca84407f49a2412391b00261c235f0993fba
Ton Lima <tondrums@hotmail.com>
2022-06-28 11:04:30 -0300

    Quantos pagamentos foram feitos entre 01/07/2005 e 22/08/2005?

--- back-end/bloco-20-Introdução-à-SQL/dia-3-filtrando-dados-de-forma-especifica/filtrandoDadosCom_IN_BETWEEN/exercicio7.sql
+++ back-end/bloco-20-Introdução-à-SQL/dia-3-filtrando-dados-de-forma-especifica/filtrandoDadosCom_IN_BETWEEN/exercicio7.sql
@@ -1,3 +1,2 @@
-SELECT * FROM payment
-WHERE payment_date BETWEEN  '2005-07-01' AND '2005-08-22'
-ORDER BY payment_date LIMIT 100;
+SELECT COUNT(*) Pagamentos FROM payment
+WHERE payment_date BETWEEN '2005-07-01' AND '2005-08-22';
