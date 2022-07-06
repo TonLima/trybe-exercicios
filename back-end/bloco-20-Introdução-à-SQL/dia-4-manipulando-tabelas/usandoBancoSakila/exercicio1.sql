e8f099ff6b81be7771540b1f2927f5ca707b1f50
Ton Lima <tondrums@hotmail.com>
2022-07-06 17:27:16 -0300

    Insira um novo funcionário na tabela sakila.staff. Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione Table Inspector. Clique na aba columns e verifique quais campos aceitam nulos para te guiar. Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa exploração

---
+++ back-end/bloco-20-Introdução-à-SQL/dia-4-manipulando-tabelas/usandoBancoSakila/exercicio1.sql
@@ -0,0 +1,4 @@
+-- Active: 1654631288725@@127.0.0.1@3306@sakilaSELECT * FROM staff;
+
+INSERT INTO staff (first_name, last_name, address_id, email, store_id, active, username, password, last_update)
+VALUES ('Washington', 'Ferreira', '2', 'tondrums@hotmail.com', '1', '1', 'Ton', 'casanova2013','2022-07-06');
