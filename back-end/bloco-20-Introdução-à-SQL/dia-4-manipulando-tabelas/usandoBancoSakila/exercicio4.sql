08df3d5b8e6c03714baf6073199db8b823576c25
Ton Lima <tondrums@hotmail.com>
2022-07-06 17:56:30 -0300

    Cadastre três categorias de uma vez só na tabela sakila.category.

---
+++ back-end/bloco-20-Introdução-à-SQL/dia-4-manipulando-tabelas/usandoBancoSakila/exercicio4.sql
@@ -0,0 +1,6 @@
+SELECT * FROM category;
+
+INSERT INTO category (name, last_update)
+VALUES ('Samba', '2022-07-06 18:15:32'),
+       ('Sertanejo', '2022-07-06 18:19:00'),
+       ('Brega', '2022-07-06 18:18:32');
