7b1f82f48eb087a0bd91c35751a49f3704f0b661
Ton Lima <tondrums@hotmail.com>
2022-07-06 17:33:01 -0300

    Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query.

---
+++ back-end/bloco-20-Introdução-à-SQL/dia-4-manipulando-tabelas/usandoBancoSakila/exercicio2.sql
@@ -0,0 +1,3 @@
+INSERT INTO staff (first_name, last_name, address_id, email, store_id, active, username, password, last_update)
+VALUES ('Heitor', 'Ferreira', '2', 'heitor@hotmail.com', '1', '1', 'Heitor', '#123456','2022-07-06 17:36:00'),
+('Isabella', 'Ferreira', '2', 'isabella@hotmail.com', '1', '1', 'Isa', '#010203','2022-07-06 17:40:00');
