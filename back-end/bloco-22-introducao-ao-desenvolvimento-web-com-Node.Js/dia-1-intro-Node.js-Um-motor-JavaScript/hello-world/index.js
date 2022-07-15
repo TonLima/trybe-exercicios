737497a9f70cff7785042d1c7d856d6a2d7107a4
Ton Lima <tondrums@hotmail.com>
2022-07-15 19:27:00 -0300

    Usando uma biblioteca Node.js de terceiros de nome e idade

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-Node.Js/dia-1-intro-Node.js-Um-motor-JavaScript/hello-world/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-Node.Js/dia-1-intro-Node.js-Um-motor-JavaScript/hello-world/index.js
@@ -1,1 +1,9 @@
-console.log('hello, world!');
+const readline = require('readline-sync'); //fazendo a importação de um pacote de terceiros
+
+const name = readline.question('Qual é o seu nome?');
+const age = readline.questionInt('Qual a sua idade?');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
+
+
+// console.log('hello, world!');
